fdf898108e7e0199ceb871612afc4ec8c801e0a4
Jeffrey Buitenhuis <jeffrey@onemustcode.nl>
2017-08-14 18:49:30 +0200

    Added constant to the sorting directions

--- src/Sorting/Ascending.php
+++ src/Sorting/Ascending.php
@@ -4,11 +4,13 @@ namespace OneMustCode\Query\Sorting;
 
 class Ascending extends AbstractSorting implements SortingInterface
 {
+    const DIRECTION = 'asc';
+
     /**
      * @inheritdoc
      */
     public function getDirection()
     {
-        return 'asc';
+        return self::DIRECTION;
     }
 }
--- src/Sorting/Descending.php
+++ src/Sorting/Descending.php
@@ -4,11 +4,13 @@ namespace OneMustCode\Query\Sorting;
 
 class Descending extends AbstractSorting implements SortingInterface
 {
+    const DIRECTION = 'desc';
+
     /**
      * @inheritdoc
      */
     public function getDirection()
     {
-        return 'desc';
+        return self::DIRECTION;
     }
 }
